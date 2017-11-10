6aec56b460ded2492be938842c189b032645ecc6
Unknown <brbodily@student.neumont.edu>
2017-11-10 16:11:23 -0700

    Set up lists for hard level

--- NimProject/src/main/java/group1/csc130/levels/HardLevel.java
+++ NimProject/src/main/java/group1/csc130/levels/HardLevel.java
@@ -1,5 +1,7 @@
 package group1.csc130.levels;
 
+import java.util.List;
+
 /**
  * 
  * @author Unholydeath
@@ -7,11 +9,54 @@ package group1.csc130.levels;
  */
 
 public class HardLevel extends Level {
+	
+	private List<Character> m_row1;
+	private List<Character> m_row2;
+	private List<Character> m_row3;
+	private List<Character> m_row4;
+	
 	public HardLevel(){
         super.setNumRows(4);
-    }
-	// private List<char> m_row1; 2 beads
-	// private List<char> m_row2; 3 beads
-	// private List<char> m_row3; 7 beads
-	// private List<char> m_row4; 8 beads
+	}
+
+	public List<Character> getM_row1() {
+		return m_row1;
+	}
+
+	public void setM_row1(int beads) {
+		for (int i = 0; i < beads; i++){
+			m_row1.add('*');
+		}
+	}
+
+	public List<Character> getM_row2() {
+		return m_row2;
+	}
+
+	public void setM_row2(int beads) {
+		for (int i = 0; i < beads; i++){
+			m_row2.add('*');
+		}
+	}
+
+	public List<Character> getM_row3() {
+		return m_row3;
+	}
+
+	public void setM_row3(int beads) {
+		for (int i = 0; i < beads; i++){
+			m_row1.add('*');
+		}
+	}
+
+	public List<Character> getM_row4() {
+		return m_row4;
+	}
+
+	public void setM_row4(int beads) {
+		for (int i = 0; i < beads; i++){
+			m_row1.add('*');
+		}
+	}
+	
 }
