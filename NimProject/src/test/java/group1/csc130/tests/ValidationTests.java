e0e562e1573e293edb545b5fc0820089f6d21aa9
TinaMoore <TMoore@student.neumont.edu>
2017-11-13 16:22:24 -0700

    test

---
+++ NimProject/src/test/java/group1/csc130/tests/ValidationTests.java
@@ -0,0 +1,14 @@
+package group1.csc130.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValidationTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+} 
