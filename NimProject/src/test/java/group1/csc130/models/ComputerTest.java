575cd5ac6a9cc54616b32ffa479c33ccf1d7d9a6
Unknown <brbodily@student.neumont.edu>
2017-11-14 09:33:41 -0700

    Some Computer Unit tests
    Only tested valid ranges for chooseRow and chooseBeads.

--- NimProject/src/test/java/group1/csc130/models/ComputerTest.java
+++ NimProject/src/test/java/group1/csc130/models/ComputerTest.java
@@ -5,21 +5,48 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ComputerTest {
-    @Test
-    public void makeRowChoice() throws Exception {
-
-    }
-
-    @Test
-    public void makeBeadChoice() throws Exception {
-    }
-
-    @Test
-    public void chooseRow() throws Exception {
-    }
-
-    @Test
-    public void chooseBeads() throws Exception {
-    }
-
+	
+	/**
+	 * Returns whether the chooseRow function returned a number within the given range
+	 */
+	@Test
+	public void TestChooseRow(){
+		Computer comp = new Computer();
+		int maxRows = 3;
+		int row = comp.chooseRow(maxRows);
+		assertTrue(row >= 1 && row <= maxRows);
+	}
+	
+	/**
+	 * Returns whether the chooseRow function returned a number within the given range
+	 */
+	@Test
+	public void Test2ChooseRow(){
+		Computer comp = new Computer();
+		int maxRows = 6;
+		int row = comp.chooseRow(maxRows);
+		assertTrue(row >= 1 && row <= maxRows);
+	}
+	
+	/**
+	 * Returns whether the chooseBeads function returned a number within the given range
+	 */
+	@Test
+	public void TestChooseBeads(){
+		Computer comp = new Computer();
+		int range = 5;
+		int beads = comp.chooseBeads(range);
+		assertTrue(beads >= 1 && beads <= range);
+	}
+	
+	/**
+	 * Returns whether the chooseBeads function returned a number within the given range
+	 */
+	@Test
+	public void Test2ChooseBeads(){
+		Computer comp = new Computer();
+		int range = 7;
+		int beads = comp.chooseBeads(range);
+		assertTrue(beads >= 1 && beads <= range);
+	}
 }
