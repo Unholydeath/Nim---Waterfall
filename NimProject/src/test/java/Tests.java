a1e1ed57ccf32bb86cc34ee23d1966f6e75025fb
Unknown <isaiahcjc5@gmail.com>
2017-11-13 21:49:43 -0700

    Tests

---
+++ NimProject/src/test/java/Tests.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Tests {
+
+    @Test
+    public void testGoodInputOnNotPlayingAgain(){
+          Assert.assertTrue();
+    }
+
+
+}
