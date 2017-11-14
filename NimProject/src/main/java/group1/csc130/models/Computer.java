6294d639b1ca575af391df7128b7403b4a414b2b
Unknown <brbodily@student.neumont.edu>
2017-11-13 19:26:30 -0700

    Computer and Player documentation

--- NimProject/src/main/java/group1/csc130/models/Computer.java
+++ NimProject/src/main/java/group1/csc130/models/Computer.java
@@ -4,48 +4,54 @@ import java.util.Random;
 
 public class Computer extends Player {
 	
-	public int maxRowRange;
-	public int maxBeadRange;
-	
 	private Random m_random;
 	
+	/**
+	 * Instantiates a random object, sets the player's name to "Computer", and sets the PlayerType to COMPUTER
+	 */
 	public Computer(){
 		m_random = new Random();
 		setName("Computer");
 		setType(PlayerType.COMPUTER);
 	}
 	
+	/**
+	 * Inherited from Player to allow access to the chooseRow(numRows) random generator.
+	 * Chooses a random number between 1 and the number of rows given.
+	 * @param int numRows - Maximum number of rows to pass to chooseRow(numRows)
+	 * @return int chosenRow - Number of the chosen row
+	 */
 	@Override
 	public int makeRowChoice(int numRows){
 		return chooseRow(numRows);
 	}
 	
+	/**
+	 * Inherited from Player to allow access to the chooseBeads(maxBeads) random generator
+	 * Chooses a random number between 1 and the number of beads given
+	 * @param int maxBeads - Maximum number of beads to pass to chooseBeads(maxBeads)
+	 * @return int chosenBeads - Number of beads chosen
+	 */
 	@Override
 	public int makeBeadChoice(int maxBeads){
 		return chooseBeads(maxBeads);
 	}
 	
+	/**
+	 * Selects a row from a randomly generated number based on the maximum number of rows available
+	 * @param int range - Maximum range for the random generator
+	 * @return int selectedRow - Returns the number from the random generator
+	 */
 	public int chooseRow(int range){
 		return m_random.nextInt(range) + 1;
 	}
 	
+	/**
+	 * Returns a randomly generated number of beads based on the maximum number of beads avaiable
+	 * @param int range - Maximum range for the random generator
+	 * @return int selectedBeads - Returns the number from the random generator
+	 */
 	public int chooseBeads(int range){
         return m_random.nextInt(range) + 1;
 	}
-
-    public int getMaxRowRange() {
-        return maxRowRange;
-    }
-
-    public void setMaxRowRange(int maxRowRange) {
-        this.maxRowRange = maxRowRange;
-    }
-
-    public int getMaxBeadRange() {
-        return maxBeadRange;
-    }
-
-    public void setMaxBeadRange(int maxBeadRange) {
-        this.maxBeadRange = maxBeadRange;
-    }
 }
--- NimProject/src/main/java/group1/csc130/models/Player.java
+++ NimProject/src/main/java/group1/csc130/models/Player.java
@@ -9,31 +9,65 @@ public class Player {
     private String m_name;
     private PlayerType m_type;
 	
+    /**
+     * Instantiates player and sets the PlayerType to PLAYER
+     */
 	public Player(){
 		setType(PlayerType.PLAYER);	
 	}
 	
+	/**
+	 * Sets m_name to the given string
+	 * @param String newName - Name of the player
+	 */
 	public Player(String newName){
 		setName(newName);
 	}
 
+	/**
+	 * Returns the String m_name
+	 * @return String m_name - Name of the player 
+	 */
     public String getName() {
         return m_name;
     }
 
+    /**
+     * Sets m_name to a given String
+     * @param String name - String that'll become the new name
+     */
     public void setName(String name) {
         m_name = name;
     }
 
+    /**
+     * Returns the player's PlayerType
+     * @return PlayerType m_type - Type of the player
+     */
 	public PlayerType getType() {
 		return m_type;
 	}
 
+	/**
+	 * Set m_type to the given PlayerType given
+	 * @param PlayerType m_type - PlayerType to set m_type to
+	 */
 	public void setType(PlayerType m_type) {
 		this.m_type = m_type;
 	}
 	
+	/**
+	 * Give a maximum number of rows and receive a row chosen at random
+	 * @param int numRows - Max number of rows, to be used for Computer's random generator
+	 * @return int selectedRow - Number of selected row
+	 */
 	public int makeRowChoice(int numRows){ return 0;}
 	
+	
+	/**
+	 * Give a maximum number of beads and receive a number of beads chosen at random
+	 * @param maxBeads - Max number of beads in a predetermined row
+	 * @return int numBeads - Number of beads selected
+	 */
 	public int makeBeadChoice(int maxBeads){ return 0;}
 }
