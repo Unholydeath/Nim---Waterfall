bd7a99b6b149872eab7401d6f68ef243d23f76c7
Unknown <brbodily@student.neumont.edu>
2017-11-14 00:51:25 -0700

    Almost all easy tests

--- NimProject/src/test/java/group1/csc130/levels/EasyLevelTest.java
+++ NimProject/src/test/java/group1/csc130/levels/EasyLevelTest.java
@@ -5,40 +5,245 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EasyLevelTest {
+	
+	/**
+	 * Should return 2, the number of rows of an easy level board
+	 */
+	@Test
+	public void TestGetNumRows(){
+		Level level = new EasyLevel();
+		assertEquals((int)2, level.getNumRows());
+	}
+	
+	/**
+	 * Should return 3 as the initial row 1 value
+	 */
+	@Test
+	public void TestInitaialRow1Value(){
+		Level level = new EasyLevel();
+		assertEquals((int)3, level.getBeadAmount(1));
+	}
+	
+	/**
+	 * Should remove 2 beads from row 1, leaving 2 beads on that row
+	 */
     @Test
-    public void getRow1() throws Exception {
+    public void TestRemoveBeadsFromRow1(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(1, 2);
+    	assertEquals((int)1, level.getBeadAmount(1));
     }
-
+    
+    /**
+     * Should remove all 3 beads from row 1, leaving 0 beads on that row
+     */
     @Test
-    public void setRow1() throws Exception {
+    public void TestRemovingAllBeadsFromRow1(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(1, 3);
+    	assertEquals((int)0, level.getBeadAmount(1));
     }
-
+    
+    /**
+     * Should remove 0 beads from row 1, leaving all 3 beads on the row
+     */
     @Test
-    public void getRow2() throws Exception {
+    public void TestRemovingZeroBeadsFromRow1(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(1, 0);
+    	assertEquals((int)3, level.getBeadAmount(1));
     }
-
+    
+    /**
+     * Because it asks to remove too many beads, the function will not remove any beads
+     * and will not crash the program
+     */
     @Test
-    public void setRow2() throws Exception {
+    public void TestRemovingTooManyBeadsFromRow1(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(1, 5);
+    	assertEquals((int)3, level.getBeadAmount(1));
+    }
+    
+    /**
+     * The function shouldn't remove any beads and should return the current number of beads.
+     * The function will not crash the program
+     */
+    @Test
+    public void TestRemovingNegativeBeadsFromRow1(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(1, -3);
+    	assertEquals((int)3, level.getBeadAmount(1));
+    }
+    
+    /**
+     * Should return 3, the initial value of beads in row 2
+     */
+    @Test
+    public void TestInitialRow2Value(){
+    	Level level = new EasyLevel();
+    	assertEquals((int)3, level.getBeadAmount(2));
+    }
+	
+    /**
+	 * Should remove 1 bead from row 2, leaving 2 beads on that row
+	 */
+    @Test
+    public void TestRemoveBeadsFromRow2(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(2, 1);
+    	assertEquals((int)2, level.getBeadAmount(2));
+    }
+    
+    /**
+     * Should remove all 3 beads from row 2, leaving 0 beads on that row
+     */
+    @Test
+    public void TestRemovingAllBeadsFromRow2(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(2, 3);
+    	assertEquals((int)0, level.getBeadAmount(2));
+    }
+    
+    /**
+     * Should remove 0 beads from row 2, leaving all 3 beads on the row
+     */
+    @Test
+    public void TestRemovingZeroBeadsFromRow2(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(2, 0);
+    	assertEquals((int)3, level.getBeadAmount(2));
+    }
+    
+    /**
+     * Because it asks to remove too many beads, the function will not remove any beads
+     * and will not crash the program
+     */
+    @Test
+    public void TestRemovingTooManyBeadsFromRow2(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(2, 5);
+    	assertEquals((int)3, level.getBeadAmount(2));
+    }
+    
+    /**
+     * The function shouldn't remove any beads and should return the current number of beads.
+     * The function will not crash the program
+     */
+    @Test
+    public void TestRemovingNegativeBeadsFromRow2(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(2, -3);
+    	assertEquals((int)3, level.getBeadAmount(2));
+    }
+   
+    /**
+     * The function should return 3, the new amount that was given
+     * The function SHOULD FAIL because the setRow function doesn't clear the list
+     */
+    @Test
+    public void TestSettingRow1To3(){
+    	EasyLevel easy = new EasyLevel();
+    	easy.setRow1(3);
+    	assertEquals((int)3, easy.getBeadAmount(1));
+    }
+    
+    /**
+     * The function should return 0, the desired amount
+     * The function SHOULD FAIL because the setRow function doesn't clear the list when called
+     */
+    @Test
+    public void TestSettingRow1To0(){
+    	EasyLevel easy = new EasyLevel();
+    	easy.setRow1(0);
+    	assertEquals((int)0, easy.getBeadAmount(1));
+    }
+    
+    /**
+     * The function doesn't allow for negatives, so the functions shouldn't change the original value
+     */
+    @Test
+    public void TestSettingRow1ToNegative(){
+    	EasyLevel easy = new EasyLevel();
+    	int currentBeads = easy.getBeadAmount(1);
+    	easy.setRow1(-6);
+    	assertEquals(currentBeads, easy.getBeadAmount(1));
+    }
+    
+    /**
+     * The function should return 3, the new amount that was given
+     * The function SHOULD FAIL because the setRow function doesn't clear the list
+     */
+    @Test
+    public void TestSettingRow2To3(){
+    	EasyLevel easy = new EasyLevel();
+    	easy.setRow2(3);
+    	assertEquals((int)3, easy.getBeadAmount(2));
+    }
+    
+    /**
+     * The function should return 0, the desired amount
+     * The function SHOULD FAIL because the setRow function doesn't clear the list when called
+     */
+    @Test
+    public void TestSettingRow2To0(){
+    	EasyLevel easy = new EasyLevel();
+    	easy.setRow2(0);
+    	assertEquals((int)0, easy.getBeadAmount(2));
+    }
+    
+    /**
+     * The function doesn't allow for negatives, so the functions shouldn't change the original value
+     */
+    @Test
+    public void TestSettingRow2ToNegative(){
+    	EasyLevel easy = new EasyLevel();
+    	int currentBeads = easy.getBeadAmount(2);
+    	easy.setRow2(-6);
+    	assertEquals(currentBeads, easy.getBeadAmount(2));
+    }
+    
+    /**
+     * WinCheck should return true since both lists are empty
+     */
+    @Test
+    public void TestPositiveWinCheck(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(1, 3);
+    	level.removeBeads(2, 3);
+    	assertTrue(level.WinCheck());
+    }
+    
+    /**
+     * WinCheck should return false since only one list is empty
+     */
+    @Test
+    public void TestWinCheckRow1EmptyRow2HasBeads(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(1, 3);
+    	assertFalse(level.WinCheck());
+    }
+    
+    /**
+     * WinCheck should return false since only one list is empty
+     */
+    @Test
+    public void TestWinCheckRow1HasBeadsRow2Empty(){
+    	Level level = new EasyLevel();
+    	level.removeBeads(2, 3);
+    	assertFalse(level.WinCheck());
+    }
+    
+    /**
+     * WinCheck should return false since neither list is empty
+     */
+    @Test
+    public void TestWinCheckRow1HasBeadsRow2HasBeads(){
+    	Level level = new EasyLevel();
+    	assertFalse(level.WinCheck());
     }
 
     @Test
     public void printBoard() throws Exception {
     }
-
-    @Test
-    public void getNumRows() throws Exception {
-    }
-
-    @Test
-    public void getBeadAmount() throws Exception {
-    }
-
-    @Test
-    public void removeBeads() throws Exception {
-    }
-
-    @Test
-    public void winCheck() throws Exception {
-    }
-
 }
