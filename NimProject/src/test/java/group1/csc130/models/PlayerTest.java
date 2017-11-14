b4f395e309ca40a3adf0953e34bcaed06d45df2b
Unknown <brbodily@student.neumont.edu>
2017-11-14 09:16:59 -0700

    Player Unit tests

--- NimProject/src/test/java/group1/csc130/models/PlayerTest.java
+++ NimProject/src/test/java/group1/csc130/models/PlayerTest.java
@@ -2,8 +2,9 @@ package group1.csc130.models;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import group1.csc130.models.Player.PlayerType;
 
+<<<<<<< Updated upstream
 public class PlayerTest {
     Player player = new Player();
     @Test
@@ -15,21 +16,76 @@ public class PlayerTest {
     @Test
     public void setName() throws Exception {
     }
+=======
+import static org.junit.Assert.*;
+>>>>>>> Stashed changes
 
+/**
+ * Contains the testing methods of the Player class
+ * 
+ * @author Unholydeath
+ *
+ */
+
+public class PlayerTest {
+	
+	/**
+	 * Tests whether the initial PlayerType of Player is PLAYER when instantiated
+	 */
+	@Test
+	public void TestPlayerIntialType(){
+		Player player = new Player();
+		assertTrue(player.getType() == PlayerType.PLAYER);
+	}
+	
+	/**
+	 * Tests whether the setType() function of Player can change the PlayerType of the Player
+	 */
+	@Test
+	public void TestPlayerSetPlayerType(){
+		Player player = new Player();
+		player.setType(PlayerType.COMPUTER);
+		assertTrue(player.getType() == PlayerType.COMPUTER);
+	}
+	
+	/**
+	 * Tests whether the setType() function of Player can be called multipul times and hold the final desired type of PlayerType
+	 */
+	@Test
+	public void TestPlayerSetPlayerTypeTwice(){
+		Player player = new Player();
+		player.setType(PlayerType.COMPUTER);
+		player.setType(PlayerType.PLAYER);
+		assertTrue(player.getType() == PlayerType.PLAYER);
+	}
+	
+	/**
+	 * Player's name isn't instantiated when the class is, so the name should return null
+	 */
     @Test
-    public void getType() throws Exception {
+    public void TestInitialNameOfPlayer() {
+    	Player player = new Player();
+    	assertTrue(player.getName() == null);
     }
-
+    
+    /**
+     * Tests if the setName function actually changes the Player's name
+     */
     @Test
-    public void setType() throws Exception {
+    public void TestSetNameOfPlayer(){
+    	Player player = new Player();
+    	player.setName("BillyBob");
+    	assertTrue(player.getName().compareTo("BillyBob") == 0);
     }
-
+    
+    /**
+     * Tests if the setName function changes the name properly multipul times
+     */
     @Test
-    public void makeRowChoice() throws Exception {
+    public void TestSetNameOfPlayerTwice(){
+    	Player player = new Player();
+    	player.setName("Steve");
+    	player.setName("Joe");
+    	assertTrue(player.getName().compareTo("Joe") == 0);
     }
-
-    @Test
-    public void makeBeadChoice() throws Exception {
-    }
-
 }
